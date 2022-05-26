191bbd4f6e2ba2236f10cfc6a9d65d7a437d2ad0
chuqian zhou <zcq.cndk@gmail.com>
2022-05-26 17:14:28 +0800

    add extract relative poses

--- prepare_dataset/examples/extract_poses.cpp
+++ prepare_dataset/examples/extract_poses.cpp
@@ -17,12 +17,14 @@ int main(int argc, char** argv) {
 
     cch::GpsData cur_gps_data;
 
-    Eigen::Vector3d relative_pos; 
+    Eigen::Vector3d rel_pos; 
+    Eigen::Vector3d abs_pos; 
     Eigen::Vector3d pre_pos;
     Eigen::Vector3d cur_pos;        
     Eigen::Vector3d init_pos;
 
-    Eigen::Quaterniond relative_q;
+    Eigen::Quaterniond rel_q;
+    Eigen::Quaterniond abs_q;
     Eigen::Quaterniond pre_q;
     Eigen::Quaterniond cur_q;
     Eigen::Quaterniond init_q;
@@ -30,7 +32,8 @@ int main(int argc, char** argv) {
     std::ofstream path_to_pose_file(argv[3]);
     std::ofstream path_to_translation_file(argv[4]);
 
-    bool is_init_frame = true;
+    bool is_init_frame_absolute = true;
+    bool is_init_frame_relative = true;
     while (std::getline(timestamp_file, cur_timestamp)) {
         size_t begin = cur_timestamp.find_first_of(' ');
         size_t end = cur_timestamp.find_last_of('.');
@@ -40,38 +43,57 @@ int main(int argc, char** argv) {
 
         // Get the nearest gps data for current timestamp
         gps_handler.timestamp2Gps(cur_timestamp, cur_gps_data);
+
+        // gps data to utm pose
         gps_handler.gps2UtmPose(cur_gps_data, cur_pos, cur_q);
 
-        if (is_init_frame) {
-            is_init_frame = false;
+        if (is_init_frame_absolute) {
+            is_init_frame_absolute = false;
             init_pos = cur_pos;
             init_q = cur_q;
-            cch::getRelativePose(cur_pos, init_pos, cur_q, init_q, relative_pos, relative_q);
         }
 
-        // Get relative pose from previous frame to current frame
-        // cch::getRelativePose(pre_pos, cur_pos, pre_q, cur_q, relative_pos, relative_q);
-
-        // Get absolute pose in the first pose's frame
-        cch::getRelativePose(cur_pos, init_pos, cur_q, init_q, relative_pos, relative_q);
+        /* Get absolute pose at the first pose's frame */
+        cch::getRelativePose(cur_pos, init_pos, cur_q, init_q, abs_pos, abs_q);
 
         // Transform from gps frame to cam frame
-        relative_q = relative_q * Eigen::AngleAxisd(M_PI / 2, Eigen::Vector3d::UnitY());
+        abs_q = abs_q * Eigen::AngleAxisd(M_PI / 2, Eigen::Vector3d::UnitY());
 
+        // Write to the file
+        // path_to_pose_file << cur_timestamp << " " 
+        //                   << std::to_string(abs_pos[0]) << " " 
+        //                   << std::to_string(abs_pos[1]) << " " 
+        //                   << std::to_string(abs_pos[2]) << " "
+        //                   << std::to_string(abs_q.x()) << " " 
+        //                   << std::to_string(abs_q.y()) << " " 
+        //                   << std::to_string(abs_q.z()) << " " 
+        //                   << std::to_string(abs_q.w()) << "\n";
+
+        /* ********************************************************** */
+
+        /* Get relative pose from previous frame to current frame */
+        if (is_init_frame_relative) {
+            is_init_frame_relative = false;
+            pre_pos = abs_pos;
+            pre_q = abs_q;
+        }
+        cch::getRelativePose(pre_pos, abs_pos, pre_q, abs_q, rel_pos, rel_q);
+
+        // Write to the file
         path_to_pose_file << cur_timestamp << " " 
-                          << std::to_string(relative_pos[0]) << " " 
-                          << std::to_string(relative_pos[1]) << " " 
-                          << std::to_string(relative_pos[2]) << " "
-                          << std::to_string(relative_q.x()) << " " 
-                          << std::to_string(relative_q.y()) << " " 
-                          << std::to_string(relative_q.z()) << " " 
-                          << std::to_string(relative_q.w()) << "\n"; 
+                          << std::to_string(rel_pos[0]) << " " 
+                          << std::to_string(rel_pos[1]) << " " 
+                          << std::to_string(rel_pos[2]) << " "
+                          << std::to_string(rel_q.x()) << " " 
+                          << std::to_string(rel_q.y()) << " " 
+                          << std::to_string(rel_q.z()) << " " 
+                          << std::to_string(rel_q.w()) << "\n"; 
 
         // double translation = std::sqrt(std::pow(relative_pos[0], 2) + std::pow(relative_pos[1], 2) + std::pow(relative_pos[2], 2));
         // path_to_translation_file << std::to_string(i) << " " << std::to_string(translation) << "\n";
 
-        pre_pos = std::move(cur_pos);
-        pre_q = std::move(cur_q);
+        pre_pos = std::move(abs_pos);
+        pre_q = std::move(abs_q);
     }
 
     path_to_translation_file.close();
